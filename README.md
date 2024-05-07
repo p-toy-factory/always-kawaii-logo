6de99977e4326f2edef25963d2b3a028cd3edde6
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2024-05-07 18:18:33 +0800

    feat: support misskey and qwik websites

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Always kawaii logo
+
 A userscript to make logo of the websites kawaii if available
 
 ![Screenshot_6-5-2024_233453_react dev](https://github.com/p-toy-factory/always-kawaii-logo/assets/45930107/34ca2898-201a-4682-a6cd-e13b48db3fc1)
@@ -9,10 +10,13 @@ A userscript to make logo of the websites kawaii if available
 2. Install this userscript from [Greasy Fork](https://greasyfork.org/zh-CN/scripts/494273-always-kawaii-logo) or [GitHub](https://raw.githubusercontent.com/p-toy-factory/always-kawaii-logo/main/script.user.js)
 
 ## Supported websites
+
 - https://asahilinux.org/
 - https://bsky.app/
 - https://www.haskell.org/
 - https://hono.dev/
+- https://misskey-hub.net/
 - https://nextjs.org/
+- https://qwik.dev/
 - https://react.dev/
 - https://vuejs.org/
--- script.user.js
+++ script.user.js
@@ -1,22 +1,25 @@
 // ==UserScript==
 // @name         Always kawaii logo
 // @namespace    http://github.com/p-toy-factory/always-kawaii-logo
-// @version      1.0.0
+// @version      1.1.0
 // @description  Make logo of the websites kawaii if available
 // @author       Pink Champagne
-// @downloadURL  https://raw.githubusercontent.com/p-toy-factory/always-kawaii-logo/main/script.user.js
-// @updateURL    https://raw.githubusercontent.com/p-toy-factory/always-kawaii-logo/main/script.user.js
 // @supportURL   https://github.com/p-toy-factory/always-kawaii-logo/issues
 // @match        https://asahilinux.org/*
 // @match        https://bsky.app/*
 // @match        https://www.haskell.org/
 // @match        https://hono.dev/
-// @match        https://nextjs.org/
+// @match        https://misskey-hub.net/*
+// @match        https://nextjs.org/*
+// @match        https://qwik.dev/*
 // @match        https://react.dev/*
 // @match        https://vuejs.org/
 // @grant        none
 // @run-at       document-start
+// @downloadURL  https://raw.githubusercontent.com/p-toy-factory/always-kawaii-logo/main/script.user.js
+// @updateURL    https://raw.githubusercontent.com/p-toy-factory/always-kawaii-logo/main/script.user.js
 // ==/UserScript==
+// @ts-check
 (function () {
   "use strict";
 
@@ -39,8 +42,8 @@
   switch (location.hostname) {
     case "asahilinux.org": {
       if (
-        localStorage.getItem("kawaii") === String(true) ||
-        location.search.includes(kawaiiSearch)
+        location.search.includes(kawaiiSearch) ||
+        localStorage.getItem("kawaii") === String(true)
       ) {
         return;
       }
@@ -50,8 +53,9 @@
 
     case "bsky.app": {
       if (
-        JSON.parse(localStorage.getItem("BSKY_STORAGE"))?.kawaii === true ||
-        location.search.includes(kawaiiSearch)
+        location.search.includes(kawaiiSearch) ||
+        JSON.parse(localStorage.getItem("BSKY_STORAGE") ?? "null")?.kawaii ===
+          true
       ) {
         return;
       }
@@ -60,26 +64,60 @@
     }
 
     case "www.haskell.org":
-    case "nextjs.org": // TODO: Enable uwu on any page
     case "vuejs.org":
       enableUwUOnlyHomepage();
+      break;
+
+    case "misskey-hub.net":
+      if (/^\/[\w-]+\/$/.test(location.pathname)) {
+        if (
+          location.search.includes(uwuSearch) ||
+          localStorage.getItem("miHub_uwu") === String(true)
+        ) {
+          return;
+        }
+        location.search = uwuSearch;
+      }
+      break;
+
+    case "nextjs.org":
+      requestIdleCallback(() => {
+        if (
+          location.search.includes(uwuSearch) ||
+          document.documentElement.classList.contains("uwu")
+        ) {
+          return;
+        }
+        location.search = uwuSearch;
+      });
       break;
 
     case "hono.dev":
       enableKawaiiOnlyHomepage();
       break;
 
-    case "react.dev": {
-      if (
-        localStorage.getItem("uwu") === String(true) ||
-        location.search.includes(uwuSearch)
-      ) {
-        return;
+    case "qwik.dev":
+      {
+        if (
+          location.search.includes(uwuSearch) ||
+          localStorage.getItem("uwu") === String(true)
+        ) {
+          return;
+        }
+        location.search = uwuSearch;
       }
-      location.search = uwuSearch;
       break;
-    }
 
-    default:
+    case "react.dev":
+      {
+        if (
+          location.search.includes(uwuSearch) ||
+          localStorage.getItem("uwu") === String(true)
+        ) {
+          return;
+        }
+        location.search = uwuSearch;
+      }
+      break;
   }
 })();
